0956318a74a8990e82305c2687c88a29994f4d32
himjl <mil@mit.edu>
2025-08-24 16:02:38 -0400

    psykit-browser

--- psykit/_internal/compilers/html_rendering.py
+++ psykit/_internal/compilers/html_rendering.py
@@ -4,15 +4,15 @@ import jinja2
 
 from psykit._internal.models.node_engine.node_graph import NodeGraph
 import pydantic
-import importlib
 
 
 # %%
 class CompileHtmlOptions(pydantic.BaseModel):
-    endpoint_url: pydantic.AnyHttpUrl | None = pydantic.Field(
+    event_submission_url: str | None = pydantic.Field(
         description='An endpoint to which Events will be sent. If None, the Events will simply be shown at the end of the Run.',
         default = None,
     )
+    start_node_execution_index: int
 
 
 def html(
@@ -21,17 +21,18 @@ def html(
 ) -> str:
     if options is None:
         options = CompileHtmlOptions(
-            endpoint_url=None,
+            event_submission_url=None,
+            start_node_execution_index=0,
         )
 
     # Render the node sequence using a Jinja2 template
-    template_location = Path(__file__).parent / 'node_engine_template.j2'
+    template_location = Path(__file__).parent / 'node_graph_site_template.j2'
     template = jinja2.Environment(loader=jinja2.FileSystemLoader(template_location.parent)).get_template(template_location.name)
 
     html_string = template.render(
         dict(
             node_graph=node_graph.model_dump(mode='json'),
-            event_endpoint_url=options.endpoint_url,
+            options=options.model_dump(mode='json'),
         )
     )
 
