74b0ee8918ee92b45a24be078a6472a5fafbbb3e
himjl <mil@mit.edu>
2025-08-24 19:15:35 -0400

    simplify

--- psykit/_internal/compilers/psykit-browser/src/main.ts
+++ psykit/_internal/compilers/psykit-browser/src/main.ts
@@ -15,21 +15,9 @@ type SendEventResult = {
     ok: boolean,
 }
 
-type QueuedEvent = {
-    event: Event,
-    resolve: (result: SendEventResult) => void,
-    reject: (error: Error) => void,
-    attempts: number,
-}
-
-
 export class EventClient {
     private readonly connectionUrl: string;
     private readonly runId: UUID;
-    private readonly queue: QueuedEvent[] = [];
-    private flushing = false;
-
-    private readonly maxRetries = 5;
 
     constructor(
         runId: string,
@@ -39,70 +27,7 @@ export class EventClient {
         this.runId = runId as UUID;
     }
 
-    private async queueEvent(event: Event): Promise<SendEventResult> {
-        /*
-        Enqueues an event to be sent. Returns a promise that resolves when the event is sent (or fails).
-         */
-        return new Promise<SendEventResult>(
-            (resolve, reject) => {
-                this.queue.push(
-                    {
-                        event: event,
-                        resolve: resolve,
-                        reject: reject,
-                        attempts: 0,
-                    }
-                );
-                this._maybeFlushNext();
-            }
-        );
-    }
-
-    private _maybeFlushNext() {
-        // Flushes the next event in the queue if not already flushing. Once done, calls _maybeFlushNext again to continue processing the queue.
-        if (this.flushing) {
-            return;
-        }
-
-        const nextQueuedEvent = this.queue.shift();
-        if (!nextQueuedEvent) {
-            // No events left in the queue
-            return;
-        }
-
-        // Start flushing:
-        this.flushing = true;
-        console.log(`Flushing event: ${nextQueuedEvent.event.event_type} (attempt ${nextQueuedEvent.attempts + 1})`);
-        let postEventPromise = this._postEvent(nextQueuedEvent.event);
-
-        // Handle the result of the post attempt:
-        postEventPromise
-            .then(result => {
-                nextQueuedEvent.resolve(result)
-
-                // Continue processing the queue:
-                this.flushing = false;
-                this._maybeFlushNext();
-            })
-            .catch(_err => {
-                nextQueuedEvent.attempts += 1;
-                if (nextQueuedEvent.attempts >= this.maxRetries) {
-                    nextQueuedEvent.reject(new Error(`Retry limit exceeded after ${this.maxRetries} retries`,));
-                } else {
-                    const backoffTimeMsec = Math.pow(2, nextQueuedEvent.attempts) * 100;
-                    setTimeout(
-                        () => {
-                            this.queue.unshift(nextQueuedEvent);
-                            this.flushing = false;
-                            this._maybeFlushNext();
-                        },
-                        backoffTimeMsec
-                    );
-                }
-            })
-    }
-
-    private async _postEvent(event: Event): Promise<SendEventResult> {
+    private async postEventCore(event: Event): Promise<SendEventResult> {
         /*
          This method posts an event to the server
          */
@@ -120,7 +45,6 @@ export class EventClient {
                     method: 'POST',
                     body: JSON.stringify(event),
                     headers: {'Content-Type': 'application/json'},
-                    keepalive: true,
                     signal: controller.signal,
                 }
             );
@@ -170,7 +94,8 @@ export class EventClient {
             event_payload: {},
             event_timestamp: this.getTimestamp(),
         }
-        return this.queueEvent(startEvent)
+        console.log('start event', startEvent)
+        return this.postEventCore(startEvent)
     }
 
     async sendNodeResultEvent(
@@ -183,7 +108,8 @@ export class EventClient {
             event_payload: nodeResult,
             event_timestamp: this.getTimestamp(),
         }
-        return this.queueEvent(reportEvent);
+        console.log('start event', reportEvent)
+        return this.postEventCore(reportEvent);
     }
 
     async sendEndEvent(): Promise<SendEventResult> {
@@ -194,7 +120,8 @@ export class EventClient {
             event_payload: {},
             event_timestamp: this.getTimestamp(),
         }
-        return this.queueEvent(endEvent)
+        console.log('end event', endEvent)
+        return this.postEventCore(endEvent)
     }
 }
 
--- psykit/_internal/compilers/psykit-browser/src/types.ts
+++ psykit/_internal/compilers/psykit-browser/src/types.ts
@@ -3,12 +3,12 @@ export type UUID = string & { __brand: 'UUID' };
 
 
 // NodeResult:
-export type NodeResult = {
+export type NodeResult = { // todo flow from NodePlayer
     node_id: UUID,
     timestamp_start: ISO8601,
     timestamp_end: ISO8601,
     node_execution_index: number, // 0 for first execution, 1 for second, etc.
-    action: any, // todo flow from NodePlayer
+    action: any,
     runtime_metrics: any
 }
 
@@ -34,5 +34,4 @@ export type Event = StartEvent | EndEvent | NodeResultEvent;
 export type SubmitEventResponse = {
     redirect_url: string | null;
 }
-
 
