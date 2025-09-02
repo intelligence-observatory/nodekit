cf4b8356ec75f21d40d58be8a34249f1b3c80441
himjl <mil@mit.edu>
2025-09-01 21:33:20 -0400

    rebuilt

--- nodekit-browser/vite.config.ts
+++ nodekit-browser/vite.config.ts
@@ -4,7 +4,7 @@ export default defineConfig({
     build: {
         lib: {
             entry: 'src/main.ts',
-            fileName: () => 'psykit.js',
+            fileName: () => 'nodekit.js',
             name: 'EventClient',
             formats: ['umd'], // Exports a global "EventClient" variable
         },
