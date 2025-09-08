7ff03435be20a738b5a11e92e214f6a2db4612be
himjl <mil@mit.edu>
2025-09-08 12:25:32 -0400

    remove base

--- nodekit/_internal/types/reinforcer_maps/reinforcer_maps.py
+++ nodekit/_internal/types/reinforcer_maps/reinforcer_maps.py
@@ -1,12 +1,23 @@
-from typing import Literal
+from abc import ABC
+from typing import Literal, Any
 
 from nodekit._internal.types.base import DslModel, NullParameters
-from nodekit._internal.types.reinforcer_maps.base import BaseReinforcerMap
+from nodekit._internal.types.common import SensorId
 from nodekit._internal.types.reinforcer_maps.reinforcer import Reinforcer
 
 from typing import Annotated, Union
 
 import pydantic
+
+
+# %%
+class BaseReinforcerMap(DslModel, ABC):
+    """
+    Represents a map from a fully qualified Action emitted by a particular Sensor to an Outcome.
+    """
+    reinforcer_map_type: str
+    reinforcer_map_parameters: Any
+    sensor_id: SensorId
 
 
 # %%
