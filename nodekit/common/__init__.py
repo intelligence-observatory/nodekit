8edc1d82b147a0c48fceb47a910429ceabaa305c
himjl <mil@mit.edu>
2025-09-07 22:48:27 -0400

    remove runtimemetrics; becomes BrowserContext

--- nodekit/common/__init__.py
+++ nodekit/common/__init__.py
@@ -1,7 +1,6 @@
 __all__ = [
     # Reusable structural types used as field values in models:
     'Board',
-    'PixelArea',
     'TextContent',
     'BoardRectangle',
     'BoardLocation',
@@ -9,10 +8,6 @@ __all__ = [
 ]
 
 from nodekit._internal.types.common import TextContent, BoardRectangle, BoardLocation, Timespan
-
-from nodekit._internal.types.runtime_metrics import (
-    PixelArea,
-)
 
 from nodekit._internal.types.board import Board
 
