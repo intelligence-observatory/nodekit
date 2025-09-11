c103eb3db3bc02a6d3df9d642852ddeaa2cc65a8
Esther Alter <alters@mit.edu>
2025-09-11 13:41:52 -0400

    use test.sh to always clean up dist

---
+++ nodekit-browser/test.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Build nodekit, test it, and remove dist/
+# This is a bash script, rather than a chain && of && commands in package.json, because
+# we always want to remove dist/
+# In package.json, a sequence of commands ends as soon as one of them errors.
+# If playwright tests return errors, then the last command (`rm -r dist/`) won't execute.
+# Here, we are essentially doing a try-finally.
+
+# Try to build nodekit:
+npm run build
+# Only test if nodekit was built:
+if [ $? -eq 0 ]; then
+  npx playwright test
+fi
+# Always cleanup:
+rm -r dist/
