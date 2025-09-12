5fe3a284939c5d443b094b9fa14c33432713b043
Esther Alter <alters@mit.edu>
2025-09-12 12:38:27 -0400

    hello world

--- nodekit-browser/playwright.config.ts
+++ nodekit-browser/playwright.config.ts
@@ -29,7 +29,7 @@ export default defineConfig({
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    // baseURL: 'http://localhost:3000',
+    baseURL: 'http://localhost:3000/',
 
     /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
     trace: 'on-first-retry',
@@ -39,17 +39,23 @@ export default defineConfig({
   projects: [
     {
       name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
+      use: {
+        ...devices['Desktop Chrome'],
+      },
     },
 
     {
       name: 'firefox',
-      use: { ...devices['Desktop Firefox'] },
+      use: {
+        ...devices['Desktop Firefox'],
+      },
     },
 
     {
       name: 'webkit',
-      use: { ...devices['Desktop Safari'] },
+      use: {
+        ...devices['Desktop Safari'],
+      },
     },
 
     /* Test against mobile viewports. */
@@ -74,9 +80,13 @@ export default defineConfig({
   ],
 
   /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://localhost:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+  webServer: {
+     command: 'python3 -m http.server 3000 -b localhost',
+     stdout: "ignore",
+     stderr: "ignore",
+     cwd: 'tests',
+     url: 'http://localhost:3000/',
+     reuseExistingServer: !process.env.CI ,
+     timeout: 12000000
+  },
 });
--- nodekit-browser/tests/hello-world.spec.ts
+++ nodekit-browser/tests/hello-world.spec.ts
@@ -1,28 +1,7 @@
-import { test, expect } from '@playwright/test';
-import type {ConsoleMessage} from '@playwright/test';
+import {expect, test} from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  // Log all console messages.
-  let observations: {[key: string]: ConsoleMessage[]} = {};
-  page.on('console', message => {
-    let s = message.type();
-    if (!(s in observations)) {
-      observations[s] = [];
-    }
-    observations[s].push(message);
-  });
-
-  await page.goto("file://" + process.env.NODEKIT_INDEX_HTML);
-
-  await page.waitForTimeout(2000);
-
-  Object.entries(observations).forEach(([t, messages]) => {
-    console.log(t + ":");
-    messages.forEach(m => console.log(m));
-    console.log('');
-  })
-
-  // Expect a title "to contain" a substring.
-  // await expect(page).toHaveTitle(/NodeKit Browser/);
+test('hello world', async ({ page }) => {
+  await page.goto("./hello-world.html");
+  await expect(page).toHaveTitle(/Hello World/);
 });
 
