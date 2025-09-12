84d0b6517f5302e1026182a5105cebfd9447dc7a
Esther Alter <alters@mit.edu>
2025-09-12 15:57:53 -0400

    very wip

--- nodekit-browser/tests/video.spec.ts
+++ nodekit-browser/tests/video.spec.ts
@@ -1,69 +1,83 @@
 import {expect, test} from '@playwright/test';
 import {play} from '../src/main';
+import type {NodeGraph, NodeId} from "../src/types/node-graph";
+import type {CardId, SensorId, SpatialPoint, SpatialSize, TimePointMsec} from "../src/types/common";
 
 test('hello world', async ({ page }) => {
-    await page.goto("./index.html");
-    await page.evaluate(() => {
-        let nodeGraph = {
-            "base_payment_usd": "0.01",
-            "bonus_rules": [],
-            "description": "Test task",
-            "keywords": [],
-            "max_duration_sec": 600,
-            "nodes": [
+    const context = page.context();
+
+    const html = `<!doctype html>
+    <html lang="en">
+    <head>
+        <link rel="icon" href="data:,">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+        <title>NodeKit Browser</title>
+    </head>
+    <body>
+    <div id="app"></div>
+    <script type="module" src="../dist/nodekit.js"></script>
+    </body>
+    </html>
+    `;
+    await page.setContent(html);
+
+    await context.exposeFunction("play", play);
+
+    await page.evaluate(async () => {
+        console.log(document);
+        let nodeGraph: NodeGraph = {
+            bonus_rules: [],
+            nodes: [
                 {
-                    "node_id": "acee5f9b-996c-448d-9d3d-f9fc8e4b0c8d",
-                    "board": {
+                    node_id: "acee5f9b-996c-448d-9d3d-f9fc8e4b0c8d" as NodeId,
+                    board: {
                         "board_width_px": 768,
                         "board_height_px": 768
                     },
-                    "cards": [
+                    cards: [
                         {
-                            "card_id": "3b6f3e51-437b-495f-b1bf-9437e8dcdb25",
-                            "card_type": "VideoCard",
-                            "card_parameters": {
-                                "video_link": {
-                                    "mime_type": "video/mp4",
-                                    "sha256": "44abfe9cd09734aa445799fb98653f855ea2af0d6d7de0fff1e61a36da358b4e",
-                                    "asset_url": "https://ia803409.us.archive.org/18/items/mp4_20210502/mp4.ia.mp4"
+                            card_id: "3b6f3e51-437b-495f-b1bf-9437e8dcdb25" as CardId,
+                            card_type: "VideoCard",
+                            card_parameters: {
+                                video_link: {
+                                    mime_type: "video/mp4",
+                                    sha256: "44abfe9cd09734aa445799fb98653f855ea2af0d6d7de0fff1e61a36da358b4e",
+                                    asset_url: "https://ia803409.us.archive.org/18/items/mp4_20210502/mp4.ia.mp4"
                                 }
                             },
-                            "card_shape": {
-                                "width": 0.5,
-                                "height": 0.5
+                            card_shape: {
+                                width: 0.5 as SpatialSize,
+                                height: 0.5 as SpatialSize
                             },
-                            "card_location": {
-                                "x": 0,
-                                "y": 0
+                            card_location: {
+                                x: 0 as SpatialPoint,
+                                y: 0 as SpatialPoint
                             },
-                            "card_timespan": {
-                                "start_time_msec": 0,
-                                "end_time_msec": null
+                            card_timespan: {
+                                start_time_msec: 0 as TimePointMsec,
+                                end_time_msec: null
                             }
                         }
                     ],
-                    "sensors": [
+                    sensors: [
                         {
-                            "sensor_id": "60f7ce63-0e0e-431c-945b-07570e98d7e4",
-                            "sensor_type": "ClickSensor",
-                            "sensor_parameters": {},
-                            "sensor_timespan": {
-                                "start_time_msec": 0,
-                                "end_time_msec": null
+                            sensor_id: "60f7ce63-0e0e-431c-945b-07570e98d7e4" as SensorId,
+                            sensor_type: "ClickSensor",
+                            sensor_parameters: {},
+                            sensor_timespan: {
+                                start_time_msec: 0 as TimePointMsec,
+                                end_time_msec: null
                             },
-                            "card_id": "3b6f3e51-437b-495f-b1bf-9437e8dcdb25"
+                            card_id: "3b6f3e51-437b-495f-b1bf-9437e8dcdb25" as CardId
                         }
                     ],
-                    "reinforcer_maps": [],
-                    "effects": []
+                    reinforcer_maps: [],
+                    effects: []
                 }
             ],
-            "title": "Test task (sandbox)"
+            title: "Test task (sandbox)"
         };
 
-        window.focus();
-
-    window.onload = async () => {
         // Add focus
         let events = await play(
             nodeGraph,
@@ -72,7 +86,6 @@ test('hello world', async ({ page }) => {
             }
         )
         console.log(events)
-    };
-    })
+    });
 });
 
