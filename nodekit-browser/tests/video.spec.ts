6ed9e3e7dfade976f835436cd97a6dab0a836ab0
Esther Alter <alters@mit.edu>
2025-09-16 11:48:24 -0400

    video fails to load

--- nodekit-browser/tests/video.spec.ts
+++ nodekit-browser/tests/video.spec.ts
@@ -1,12 +1,31 @@
 import {expect, test} from '@playwright/test';
+import type {ConsoleMessage} from '@playwright/test';
 
 test('video', async ({ page }) => {
+   // Get page errors.
    let errors: Error[] = [];
    page.on('pageerror', (error) => {
       errors.push(error);
-    });
+   });
+
+   // Log all console messages.
+   const logs: {[key: string]: string[]} = {};
+   page.on('console', message => {
+      let t = message.type();
+      if (!(t in logs)) {
+            logs[t] = [];
+      }
+      let location = message.location();
+      let simpleMessage = message.text() + " line: " + location.lineNumber + " column: " + location.columnNumber
+      logs[t].push(simpleMessage);
+   });
+
    await page.goto("./video.html");
    await page.waitForLoadState('domcontentloaded');
    expect(errors).toHaveLength(0);
+   await page.waitForTimeout(2000);
+
+   // TODO make this quiet.
+   expect(logs).not.toHaveProperty('error');
 });
 
