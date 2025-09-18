d3fffbcb898694a876cc4fbce984c5e1389d7a96
himjl <mil@mit.edu>
2025-09-18 12:02:17 -0400

    clean

---
+++ nodekit/_internal/types/board/__init__.py
@@ -0,0 +1,12 @@
+import pydantic
+
+from nodekit._internal.types.common import ColorHexString
+
+
+class Board(pydantic.BaseModel):
+    board_width_px: int = pydantic.Field(default=768, gt=0)
+    board_height_px: int = pydantic.Field(default=768, gt=0)
+    background_color: ColorHexString = pydantic.Field(
+        description='Hex string representing the background color of the Board.',
+        default='#808080',
+    )
--- nodekit/_internal/types/node.py
+++ nodekit/_internal/types/node.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 
 import pydantic
 
-from nodekit._internal.types.board.board import Board
+from nodekit._internal.types.board import Board
 from nodekit._internal.types.cards.cards import Card
 from nodekit._internal.types.common import NodeId
 from nodekit._internal.types.effects.effects import Effect
@@ -11,14 +11,12 @@ from nodekit._internal.types.outcome import Outcome
 from nodekit._internal.types.sensors.sensors import Sensor
 from nodekit._internal.version import VERSION
 
-
-# %% Node
+# %%
 class Node(pydantic.BaseModel):
     class Config:
         frozen = True
 
     node_id: NodeId = pydantic.Field(default_factory=uuid4)
-
     cards: List[Card] = pydantic.Field(
         description=(
             "List of Cards placed on the Board, in back-to-front order."
@@ -31,7 +29,7 @@ class Node(pydantic.BaseModel):
     board: Board = pydantic.Field(default_factory=Board)
 
 
-# %% Timeline
+# %%
 class Timeline(pydantic.BaseModel):
     nodes: List[Node] = pydantic.Field(
         min_length=1,
