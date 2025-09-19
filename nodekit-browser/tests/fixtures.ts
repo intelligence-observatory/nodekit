f6f2008f1d2d1f5eead4c880dcf49abe9a3c13a1
Esther Alter <alters@mit.edu>
2025-09-19 13:27:41 -0400

    network throttling

--- nodekit-browser/tests/fixtures.ts
+++ nodekit-browser/tests/fixtures.ts
@@ -1,10 +1,10 @@
 import {test, expect} from '@playwright/test';
 import type {Page} from "@playwright/test";
 
-export class NodeGraphPage {
+abstract class TestPage {
     public readonly page: Page;
-    private readonly errors: Error[] = [];
-    private readonly logs: {[key: string]: string[]} = {};
+    protected readonly errors: Error[] = [];
+    protected readonly logs: {[key: string]: string[]} = {};
 
     constructor(page: Page) {
         this.page = page;
@@ -27,7 +27,32 @@ export class NodeGraphPage {
        });
     }
 
+    abstract goto(filename: string) : Promise<void>;
+
+    end() {
+        // Print logged messages:
+        console.log(JSON.stringify(this.logs, null, 4));
+    }
+}
+
+export class NodeGraphPage extends TestPage {
+    private readonly networkName: string;
+    private readonly latency: number;
+
+    constructor(page: Page, networkName: string, latency: number) {
+        super(page);
+        this.networkName = networkName;
+        this.latency = latency;
+    }
+
     async goto(filename: string) {
+        // Throttle the network.
+        console.log("Network speed: " + this.networkName)
+        await this.page.context().route("**", async (route) => {
+            // Add delay before continuing the request
+            await new Promise(resolve => setTimeout(resolve, this.latency));
+            await route.continue();
+        })
         // Go to a local file page:
         await this.page.goto('./tests/' + filename);
         await this.page.setViewportSize({width: 800, height: 800});
@@ -44,10 +69,8 @@ export class NodeGraphPage {
         expect(this.page.locator('.submit-button').first().isVisible()).toBeTruthy();
     }
 
-    async end() {
-        // Print logged messages:
-        console.log(JSON.stringify(this.logs, null, 4));
-
+    end() {
+        super.end();
         // Check for thrown errors:
         expect(this.errors).toHaveLength(0);
         // Checked for logged errors:
@@ -55,14 +78,69 @@ export class NodeGraphPage {
     }
 }
 
+export class OfflineNodeGraphPage extends TestPage {
+    async goto(filename: string) {
+        await this.page.context().setOffline(true);
+        // Fail to go to a local file page:
+        try {
+            await this.page.goto('./tests/' + filename);
+        }
+        catch(e) {
+            if (e instanceof Error) {
+                this.errors.push(e);
+            }
+        }
+    }
+
+    async end() {
+        super.end();
+        expect(this.errors.length).toBeGreaterThan(0);
+        await this.page.context().setOffline(false);
+    }
+}
+
 type Fixtures = {
-    nodeGraphPage: NodeGraphPage,
+    offlinePage: OfflineNodeGraphPage,
+    noThrottle: NodeGraphPage,
+    gprs: NodeGraphPage,
+    twoG: NodeGraphPage,
+    threeG: NodeGraphPage,
+    fourG: NodeGraphPage,
+    wifi: NodeGraphPage,
+}
+
+async function testNodeGraphPage(
+    networkName: string,
+    latency: number,
+    page: Page,
+    use: (r: NodeGraphPage) => Promise<void>
+) {
+    let nodeGraphPage =  new NodeGraphPage(page, networkName, latency);
+    await use(nodeGraphPage);
 }
 
 export const nodeGraphTest = test.extend<Fixtures>({
-    nodeGraphPage: async({ page }, use) => {
-        let nodeGraphPage =  new NodeGraphPage(page);
-        await use(nodeGraphPage);
-        await nodeGraphPage.end();
-    }
+    offlinePage: async({ page }, use) => {
+        console.log("Offline:")
+        let offlinePage = new OfflineNodeGraphPage(page);
+        await use(offlinePage);
+    },
+    noThrottle: async({ page }, use) => {
+        await testNodeGraphPage("No throttle", 0, page, use);
+    },
+    gprs: async({ page }, use) => {
+        await testNodeGraphPage("GPRS", 500, page, use);
+    },
+    twoG: async({ page }, use) => {
+        await testNodeGraphPage("2G", 300, page, use);
+    },
+    threeG: async({ page }, use) => {
+        await testNodeGraphPage("3G", 100, page, use);
+    },
+    fourG: async({ page }, use) => {
+        await testNodeGraphPage("4G", 20, page, use);
+    },
+    wifi: async({ page }, use) => {
+        await testNodeGraphPage("WiFi", 2, page, use);
+    },
 });
--- nodekit-browser/tests/video.spec.ts
+++ nodekit-browser/tests/video.spec.ts
@@ -2,16 +2,23 @@ import {expect} from '@playwright/test';
 import {nodeGraphTest} from "./fixtures";
 
 // Load a video and then click it:
-nodeGraphTest('video', async ({ nodeGraphPage }) => {
-   await nodeGraphPage.goto("video.html");
-   // Find the video:
-   let video = nodeGraphPage.page.locator('video').first();
-   await expect(video).toBeVisible();
-   // Wait slightly longer than needed for the video to load, in case Playwright introduces latency:
-   await nodeGraphPage.page.waitForTimeout(99);
-   // Click the video, which triggers the ClickSensor, which ends the node:
-   await video.click();
-   // There is only one node so the whole graph should be done:
-   await nodeGraphPage.expectNodeGraphEnded();
+nodeGraphTest('video', async ({ offlinePage, noThrottle, gprs, twoG, threeG, fourG, wifi }) => {
+   const filename = 'video.html';
+   await offlinePage.goto(filename);
+   await offlinePage.end();
+
+   for (const nodeGraphPage of [noThrottle, gprs, twoG, threeG, fourG, wifi]) {
+      await nodeGraphPage.goto(filename);
+      // Find the video:
+      let video = nodeGraphPage.page.locator('video').first();
+      await expect(video).toBeVisible();
+      // Wait slightly longer than needed for the video to load, in case Playwright introduces latency:
+      await nodeGraphPage.page.waitForTimeout(99);
+      // Click the video, which triggers the ClickSensor, which ends the node:
+      await video.click();
+      // There is only one node so the whole graph should be done:
+      await nodeGraphPage.expectNodeGraphEnded();
+      nodeGraphPage.end();
+   }
 });
 
