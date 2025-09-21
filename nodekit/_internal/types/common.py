df8ec48ffe8809e51d9970d3e65002db189da132
himjl <mil@mit.edu>
2025-09-21 13:30:06 -0400

    remove money

--- nodekit/_internal/types/common.py
+++ nodekit/_internal/types/common.py
@@ -1,40 +1,6 @@
-import datetime
-from decimal import Decimal
 from typing import Literal, Annotated
 
 import pydantic
-
-
-# %% Money
-def _ensure_monetary_amount_precision(value: str) -> str:
-    SubcentMonetaryAmountAdapter = pydantic.TypeAdapter(
-        Annotated[Decimal, pydantic.Field(decimal_places=5)]
-    )
-    d = SubcentMonetaryAmountAdapter.validate_python(value)
-    return str(d)
-
-
-MonetaryAmountUsd = Annotated[
-    str,
-    pydantic.Field(description='An arbitrary amount of money in USD, including negative amounts, represented as a string with at most five decimal places, e.g., "1.00001".'),
-    pydantic.AfterValidator(_ensure_monetary_amount_precision)
-]
-
-
-# %% Money
-def _ensure_payable_monetary_amount(value: str) -> str:
-    PayableMonetaryAmountAdapter = pydantic.TypeAdapter(
-        Annotated[Decimal, pydantic.Field(decimal_places=5)]
-    )
-    d = PayableMonetaryAmountAdapter.validate_python(value)
-    return str(d)
-
-
-PayableMonetaryAmountUsd = Annotated[
-    str,
-    pydantic.Field(description='A semi-positive amount of money in USD that is payable to a worker, represented as a string with at most two decimal places, e.g., "1.00". This amount must be at least "0.01".'),
-    pydantic.AfterValidator(_ensure_payable_monetary_amount)
-]
 
 # %% Assets
 SHA256 = Annotated[str, pydantic.Field(pattern=r'^[a-f0-9]{64}$')]
