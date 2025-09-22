e5f6a50cca7e53ff8f93129a6f994462638cf286
Esther Alter <alters@mit.edu>
2025-09-22 12:29:28 -0400

    remove test-results

--- nodekit-browser/playwright.config.ts
+++ nodekit-browser/playwright.config.ts
@@ -24,6 +24,7 @@ export default defineConfig({
   workers: process.env.CI ? 1 : undefined,
   /* Reporter to use. See https://playwright.dev/docs/test-reporters */
   reporter: 'line',
+  timeout: 5000,
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
