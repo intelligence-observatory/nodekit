61e42610b60a8b9edde3bd4c3c9e70b3b532e8e4
himjl <mil@mit.edu>
2025-10-11 21:43:13 -0400

    prototype slider

--- nodekit-browser/src/board-view/card-views/slider/slider-card-view.ts
+++ nodekit-browser/src/board-view/card-views/slider/slider-card-view.ts
@@ -7,15 +7,37 @@ import type {SliderCard} from "../../../types/cards";
  * Offers a scrollbar if the content is too long.
  */
 export class SliderCardView extends CardView<SliderCard> {
-    sliderElement: HTMLDivElement | undefined;
+    sliderElement: HTMLInputElement | undefined;
 
     async prepare(
     ) {
         // Add a slider element to the root
-        this.sliderElement = document.createElement('div');
+        this.sliderElement = document.createElement('input');
+        this.sliderElement.type = 'range';
         this.sliderElement.classList.add('slider-card');
+
+        // Set the number of bins:
+        this.sliderElement.min = '1';
+        this.sliderElement.step = '1';
+        this.sliderElement.max = (this.card.num_bins).toString();
+
+        // Set orientation:
+        if (this.card.orientation === 'horizontal')
+            this.sliderElement.classList.add('slider-card--horizontal');
+        else{
+            this.sliderElement.classList.add('slider-card--vertical');
+        }
+
+        // Set initial value to middle bin
+        const middleBin = (this.card.num_bins / 2);
+        this.sliderElement.value = middleBin.toString();
+
+        // Mount
         this.root.appendChild(this.sliderElement);
+    }
 
-
+    onStart() {
+        // Set the card to interactive
+        this.setInteractivity(true);
     }
 }
