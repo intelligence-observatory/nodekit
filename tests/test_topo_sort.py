2c7f9c6c54fd1f1f0f95214fa6268fb789797878
himjl <mil@mit.edu>
2025-10-13 13:54:43 -0400

    no images needed

--- tests/test_topo_sort.py
+++ tests/test_topo_sort.py
@@ -1,68 +1,34 @@
-from pathlib import Path
 import pytest
-import nodekit._internal.ops.topological_sorting as ts
-import nodekit as nk
 
-from typing import Dict
+import nodekit as nk
+import nodekit._internal.ops.topological_sorting as ts
 
 
 # %% Helper functions
-def generate_fixation_node(
-    image: nk.assets.ImageIdentifier,
-    x: float = 0,
-    y: float = 0,
-    w: float = 0.04,
-    h: float = 0.04,
-):
-    fixation_card = nk.cards.ImageCard(x=x, y=y, w=w, h=h, image=image)
+def generate_fixation_node() -> nk.Node:
     click_sensor = nk.sensors.ClickSensor(
         mask="ellipse",
-        x=fixation_card.x,
-        y=fixation_card.y,
-        w=fixation_card.w,
-        h=fixation_card.h,
+        x=0,
+        y=0,
+        w=0.05,
+        h=0.05,
     )
-    return nk.Node(cards=[fixation_card], sensors={"fixation": click_sensor})
+    return nk.Node(cards={}, sensors={"fixation": click_sensor})
 
 
-def generate_stimulus_node(
-    stimulus: nk.assets.ImageIdentifier,
-    x: float = 0,
-    y: float = 0,
-    w: float = 0.5,
-    h: float = 0.5,
-):
-    stimulus_card = nk.cards.ImageCard(x=x, y=y, w=w, h=h, image=stimulus)
+def generate_stimulus_node():
     timeout_sensor = nk.sensors.TimeoutSensor(timeout_msec=2000)
 
-    return nk.Node(cards=[stimulus_card], sensors={"TO": timeout_sensor})
+    return nk.Node(cards={}, sensors={"TO": timeout_sensor})
 
 
-def generate_response_node(
-    left_choice_image: nk.assets.ImageIdentifier,
-    right_choice_image: nk.assets.ImageIdentifier,
-    left_args: Dict[str, float | int] = {
-        "x": -0.25,
-        "y": -0.3,
-        "w": 0.2,
-        "h": 0.2,
-    },
-    right_args: Dict[str, float] = {
-        "x": 0.25,
-        "y": -0.3,
-        "w": 0.2,
-        "h": 0.2,
-    },
-):
-    left_card = nk.cards.ImageCard(image=left_choice_image, **left_args)
-    right_card = nk.cards.ImageCard(image=right_choice_image, **right_args)
-
-    left_sensor = nk.sensors.ClickSensor(mask="rectangle", **left_args)
-    right_sensor = nk.sensors.ClickSensor(mask="rectangle", **right_args)
+def generate_response_node():
+    left_sensor = nk.sensors.ClickSensor(mask="rectangle", x=-0.5, y=0, w=0.1, h=0.1)
+    right_sensor = nk.sensors.ClickSensor(mask="rectangle", x=0.5, y=0, w =0.1, h=0.1)
     timeout_sensor = nk.sensors.TimeoutSensor(timeout_msec=2000)
 
     return nk.Node(
-        cards=[left_card, right_card],
+        cards={},
         sensors={
             "left": left_sensor,
             "right": right_sensor,
@@ -72,68 +38,24 @@ def generate_response_node(
 
 
 def generate_positive_node():
-    positive_card = nk.cards.TextCard(
-        text="Correct!",
-        font_size=0.05,
-        x=0,
-        y=0,
-        w=0.5,
-        h=0.5,
-        background_color="#32a852",
-        text_color="#ffffff",
-        justification_horizontal="center",
-        justification_vertical="center",
-    )
-    positive_timeout_sensor = nk.sensors.TimeoutSensor(
-        timeout_msec=500,
-    )
     return nk.Node(
-        cards=[positive_card],
-        sensors={"wait": positive_timeout_sensor},
+        cards={},
+        sensors={"wait": nk.sensors.TimeoutSensor(timeout_msec=1000)},
     )
 
 
 def generate_negative_node():
-    negative_card = nk.cards.TextCard(
-        text="Incorrect.",
-        font_size=0.05,
-        x=0,
-        y=0,
-        w=0.5,
-        h=0.5,
-        background_color="#a83232",
-        text_color="#ffffff",
-        justification_horizontal="center",
-        justification_vertical="center",
-    )
-    negative_timeout_sensor = nk.sensors.TimeoutSensor(
-        timeout_msec=1000,
-    )
 
     return nk.Node(
-        cards=[negative_card],
-        sensors={"wait": negative_timeout_sensor},
+        cards={},
+        sensors={"wait": nk.sensors.TimeoutSensor(timeout_msec=5000)},
     )
 
 
 # %% Topology tests:
 
-
-@pytest.fixture(scope="module")
-def my_image_files():
-    test_dir = Path(__file__).resolve().parent
-    example_dir = test_dir.parent.parent / "example" / "example_images"
-
-    my_image_files = []
-    for path in sorted(example_dir.glob("*")):
-        image_file = nk.assets.ImageFile.from_path(path)
-        my_image_files.append(image_file)
-
-    return my_image_files
-
-
-def test_example_pass(my_image_files):
-    # %% Example 1:
+def test_example_pass():
+    # Example 1:
     # Visual structure:
     #          n[f1]
     #            |
@@ -148,22 +70,16 @@ def test_example_pass(my_image_files):
     #          n[s2]
     # s: stimulus, f:fixation, p:positive, n:negative, r: response:
 
-    # Pick images for the test:
-    fixation_img = my_image_files[0].identifier
-    left_img = my_image_files[1].identifier
-    right_img = my_image_files[2].identifier
-    stim_img = my_image_files[3].identifier
-
     # Random correct answer:
     correct = ["left"]
 
     # Generate Nodes and transitions:
     nodes = {}
-    nodes["stimulus_1"] = generate_stimulus_node(stim_img)
-    nodes["stimulus_2"] = generate_stimulus_node(stim_img)
-    nodes["response_1"] = generate_response_node(left_img, right_img)
-    nodes["fixation_1"] = generate_fixation_node(fixation_img)
-    nodes["fixation_2"] = generate_fixation_node(fixation_img)
+    nodes["stimulus_1"] = generate_stimulus_node()
+    nodes["stimulus_2"] = generate_stimulus_node()
+    nodes["response_1"] = generate_response_node()
+    nodes["fixation_1"] = generate_fixation_node()
+    nodes["fixation_2"] = generate_fixation_node()
     nodes["positive_1"] = generate_positive_node()
     nodes["negative_1"] = generate_negative_node()
 
@@ -195,7 +111,7 @@ def test_example_pass(my_image_files):
 
 
 def test_example_fail(my_image_files):
-    # %% Example 1:
+    # Example 1:
     # Visual structure:
     #          n[f1] ────┐
     #            |       |
@@ -220,11 +136,11 @@ def test_example_fail(my_image_files):
 
     # Generate Nodes and transitions:
     nodes = {}
-    nodes["stimulus_1"] = generate_stimulus_node(stim_img)
-    nodes["stimulus_2"] = generate_stimulus_node(stim_img)
-    nodes["response_1"] = generate_response_node(left_img, right_img)
-    nodes["fixation_1"] = generate_fixation_node(fixation_img)
-    nodes["fixation_2"] = generate_fixation_node(fixation_img)
+    nodes["stimulus_1"] = generate_stimulus_node()
+    nodes["stimulus_2"] = generate_stimulus_node()
+    nodes["response_1"] = generate_response_node()
+    nodes["fixation_1"] = generate_fixation_node()
+    nodes["fixation_2"] = generate_fixation_node()
     nodes["positive_1"] = generate_positive_node()
     nodes["negative_1"] = generate_negative_node()
 
@@ -243,14 +159,12 @@ def test_example_fail(my_image_files):
         ts.topological_sort(nodes, transitions)
 
 
-# %% Node, Senor and transition checks:
-
-
+# %% Node, Sensor and transition checks:
 def test_multiple_roots_tie_break():
     nodes = {
-        "A": nk.Node(cards=[], sensors={"a": nk.sensors.TimeoutSensor(timeout_msec=1)}),
-        "B": nk.Node(cards=[], sensors={"b": nk.sensors.TimeoutSensor(timeout_msec=1)}),
-        "C": nk.Node(cards=[], sensors={"c": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "A": nk.Node(cards={}, sensors={"a": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "B": nk.Node(cards={}, sensors={"b": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "C": nk.Node(cards={}, sensors={"c": nk.sensors.TimeoutSensor(timeout_msec=1)}),
     }
     transitions = {
         "A": {"a": "C"},
@@ -266,9 +180,9 @@ def test_multiple_roots_tie_break():
 
 def test_invalid_sensor_reference():
     nodes = {
-        "A": nk.Node(cards=[], sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
-        "B": nk.Node(cards=[], sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
-        "C": nk.Node(cards=[], sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "A": nk.Node(cards={}, sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "B": nk.Node(cards={}, sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
+        "C": nk.Node(cards={}, sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)}),
     }
 
     # Add transition from undefined sensor:
@@ -279,7 +193,7 @@ def test_invalid_sensor_reference():
 
 def test_invalid_transition_node():
     nodes = {
-        "A": nk.Node(cards=[], sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)})
+        "A": nk.Node(cards={}, sensors={"s": nk.sensors.TimeoutSensor(timeout_msec=1)})
     }
 
     # Add transitions with non-existent Node
