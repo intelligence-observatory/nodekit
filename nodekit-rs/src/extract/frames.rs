ccf810fa7dd2816cd5049bf23d00f93ce902707f
Esther Alter <alters@mit.edu>
2025-10-15 15:54:10 -0400

    data

--- nodekit-rs/src/extract/frames.rs
+++ nodekit-rs/src/extract/frames.rs
@@ -4,3 +4,12 @@ pub enum Frames {
     Audio(Vec<Audio>),
     Video(Vec<Video>),
 }
+
+impl Frames {
+    pub fn data(&self) -> Box<dyn Iterator<Item = &[u8]> + '_> {
+        match self {
+            Self::Audio(audio) => Box::new(audio.iter().map(|a| a.data(0))),
+            Self::Video(video) => Box::new(video.iter().map(|a| a.data(0))),
+        }
+    }
+}
