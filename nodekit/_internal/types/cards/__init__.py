32a0683a35089fe5f928ee05fff066ed5278222d
himjl <mil@mit.edu>
2025-10-15 15:08:25 -0400

    more honest typing, i guess

--- nodekit/_internal/types/cards/__init__.py
+++ nodekit/_internal/types/cards/__init__.py
@@ -99,10 +99,10 @@ class SliderCard(BaseCard):
         description="The number of discrete bins in the slider.", ge=2, default=7
     )
 
-    initial_bin_index: int = pydantic.Field(
-        description="The initial bin index that the slider is set to when it first appears.",
+    initial_bin_index: int | None = pydantic.Field(
+        description="The initial bin index that the slider is set to when it first appears. If None, defaults to the middle bin.",
         ge=0,
-        default=0,
+        default=None,
     )
 
     orientation: Literal["horizontal", "vertical"] = pydantic.Field(
