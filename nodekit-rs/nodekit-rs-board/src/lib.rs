7a604a01fc7979808e47d1b9db0db55b9c688e48
Esther Alter <alters@mit.edu>
2025-10-20 12:21:50 -0400

    removed board

--- nodekit-rs/nodekit-rs-board/src/lib.rs
+++
@@ -1,8 +0,0 @@
-//! Some constants that describe the board,
-//! a raw pixel map representation of a div in a browser window.
-
-pub const BOARD_D: usize = 768;
-pub const BOARD_D_F32: f32 = 768.;
-pub const BOARD_D_F32_HALF: f32 = 384.;
-/// Width * height * RGB
-pub type Board = [u8; BOARD_D * BOARD_D * 3];
