8b53208abd720183be7b103b6f7f8f59de2b6fda
Esther Alter <alters@mit.edu>
2025-10-23 12:17:35 -0400

    py module

--- nodekit-rs/nodekit-rs-client/src/lib.rs
+++ nodekit-rs/nodekit-rs-client/src/lib.rs
@@ -3,77 +3,82 @@ use nodekit_rs_action::Action;
 use nodekit_rs_fb::{click as click_fb, graph as graph_fb, key_press as key_press_fb};
 use pyo3::prelude::*;
 
-/// A payload that can be sent to the `nodekit-rs` app.
-#[pyclass]
-#[derive(Clone)]
-pub enum Payload {
-    Graph(String),
-    Tick(Option<Action>),
-}
+#[pymodule]
+pub mod nodekit_rs_client {
+    use super::*;
 
-/// A no-op tick.
-#[pyfunction]
-pub fn noop() -> Payload {
-    Payload::Tick(None)
-}
-
-/// Convert a serialized `Graph` into a `Payload`.
-#[pyfunction]
-pub fn graph(graph: String) -> Payload {
-    Payload::Graph(graph)
-}
-
-/// Convert (x, y) coordinates into a `Payload`.
-#[pyfunction]
-pub fn click(x: f32, y: f32) -> Payload {
-    Payload::Tick(Some(Action::Click { x, y }))
-}
-
-/// Convert a keyboard key into a `Payload`.
-#[pyfunction]
-pub fn key_press(key: String) -> Payload {
-    Payload::Tick(Some(Action::KeyPress(key)))
-}
-
-#[pyfunction]
-pub fn serialize(payload: Payload) -> Vec<u8> {
-    match payload {
-        Payload::Graph(graph) => serialize_graph(graph),
-        Payload::Tick(action) => match action {
-            Some(action) => match action {
-                Action::Click { x, y } => serialize_click(x, y),
-                Action::KeyPress(key) => serialize_key_press(key),
-                Action::Submit() => todo!("serialize submit"),
-            },
-            None => Vec::default(),
-        },
+    /// A payload that can be sent to the `nodekit-rs` app.
+    #[pyclass]
+    #[derive(Clone)]
+    pub enum Payload {
+        Graph(String),
+        Tick(Option<Action>),
     }
-}
 
-fn serialize_graph(graph: String) -> Vec<u8> {
-    let mut fbb = FlatBufferBuilder::new();
-    let payload = fbb.create_vector(graph.as_bytes());
-    let args = graph_fb::GraphArgs {
-        payload: Some(payload),
-    };
-    let offset = graph_fb::Graph::create(&mut fbb, &args);
-    graph_fb::finish_graph_buffer(&mut fbb, offset);
-    fbb.finished_data().to_vec()
-}
+    /// A no-op tick.
+    #[pyfunction]
+    pub fn noop() -> Payload {
+        Payload::Tick(None)
+    }
 
-fn serialize_click(x: f32, y: f32) -> Vec<u8> {
-    let mut fbb = FlatBufferBuilder::new();
-    let args = click_fb::ClickArgs { x, y };
-    let offset = click_fb::Click::create(&mut fbb, &args);
-    click_fb::finish_click_buffer(&mut fbb, offset);
-    fbb.finished_data().to_vec()
-}
+    /// Convert a serialized `Graph` into a `Payload`.
+    #[pyfunction]
+    pub fn graph(graph: String) -> Payload {
+        Payload::Graph(graph)
+    }
 
-fn serialize_key_press(key: String) -> Vec<u8> {
-    let mut fbb = FlatBufferBuilder::new();
-    let key = fbb.create_string(&key);
-    let args = key_press_fb::KeyPressArgs { key: Some(key) };
-    let offset = key_press_fb::KeyPress::create(&mut fbb, &args);
-    key_press_fb::finish_key_press_buffer(&mut fbb, offset);
-    fbb.finished_data().to_vec()
+    /// Convert (x, y) coordinates into a `Payload`.
+    #[pyfunction]
+    pub fn click(x: f32, y: f32) -> Payload {
+        Payload::Tick(Some(Action::Click { x, y }))
+    }
+
+    /// Convert a keyboard key into a `Payload`.
+    #[pyfunction]
+    pub fn key_press(key: String) -> Payload {
+        Payload::Tick(Some(Action::KeyPress(key)))
+    }
+
+    #[pyfunction]
+    pub fn serialize(payload: Payload) -> Vec<u8> {
+        match payload {
+            Payload::Graph(graph) => serialize_graph(graph),
+            Payload::Tick(action) => match action {
+                Some(action) => match action {
+                    Action::Click { x, y } => serialize_click(x, y),
+                    Action::KeyPress(key) => serialize_key_press(key),
+                    Action::Submit() => todo!("serialize submit"),
+                },
+                None => Vec::default(),
+            },
+        }
+    }
+
+    fn serialize_graph(graph: String) -> Vec<u8> {
+        let mut fbb = FlatBufferBuilder::new();
+        let payload = fbb.create_vector(graph.as_bytes());
+        let args = graph_fb::GraphArgs {
+            payload: Some(payload),
+        };
+        let offset = graph_fb::Graph::create(&mut fbb, &args);
+        graph_fb::finish_graph_buffer(&mut fbb, offset);
+        fbb.finished_data().to_vec()
+    }
+
+    fn serialize_click(x: f32, y: f32) -> Vec<u8> {
+        let mut fbb = FlatBufferBuilder::new();
+        let args = click_fb::ClickArgs { x, y };
+        let offset = click_fb::Click::create(&mut fbb, &args);
+        click_fb::finish_click_buffer(&mut fbb, offset);
+        fbb.finished_data().to_vec()
+    }
+
+    fn serialize_key_press(key: String) -> Vec<u8> {
+        let mut fbb = FlatBufferBuilder::new();
+        let key = fbb.create_string(&key);
+        let args = key_press_fb::KeyPressArgs { key: Some(key) };
+        let offset = key_press_fb::KeyPress::create(&mut fbb, &args);
+        key_press_fb::finish_key_press_buffer(&mut fbb, offset);
+        fbb.finished_data().to_vec()
+    }
 }
