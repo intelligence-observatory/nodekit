2366db80d0270f20131ec5d5bd66bf7951c4de95
Esther Alter <alters@mit.edu>
2025-10-24 11:19:25 -0400

    comments

--- nodekit-rs/nodekit-rs-video/src/extraction.rs
+++ nodekit-rs/nodekit-rs-video/src/extraction.rs
@@ -1,8 +1,10 @@
 /// The result of an attempt to extract a [`Frame`].
 pub enum Extraction {
+    /// We found the target frame.
     Frame {
         video: Vec<u8>,
         audio: Option<Vec<u8>>,
     },
+    /// We got to the end of the video without finding the frame.
     EndOfVideo,
 }
