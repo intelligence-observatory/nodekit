9e512dcefec14a03bbe7f0459212953d0bc5124d
Esther Alter <alters@mit.edu>
2025-10-24 12:45:10 -0400

    image

---
+++ nodekit-rs/nodekit-rs-image/src/error.rs
@@ -0,0 +1,14 @@
+use std::path::PathBuf;
+use thiserror::Error;
+
+#[derive(Debug, Error)]
+pub enum Error {
+    #[error("Failed to create {1}: {0}")]
+    OpenFile(std::io::Error, PathBuf),
+    #[error("Failed to decode {1}: {0}")]
+    Decode(png::DecodingError, PathBuf),
+    #[error("Failed to get the buffer size of {0}")]
+    BufferSize(PathBuf),
+    #[error("This image has indexed colors, which is not supported: {0}")]
+    Indexed(PathBuf),
+}
---
+++ nodekit-rs/nodekit-rs-image/src/lib.rs
@@ -0,0 +1,93 @@
+//! Convert .png files into raw RGB24 bitmaps.
+//!
+//! Image formats currently supported:
+//! - RGB24
+//! - RGBA32
+//! - Grayscale
+//! - Grayscale Alpha
+
+mod error;
+
+use bytemuck::cast_slice;
+pub use error::Error;
+use png::{ColorType, Decoder};
+use std::{fs::File, io::BufReader, path::Path};
+
+/// A raw RGB24 bitmap, a width, and a height.
+pub struct Image {
+    pub buffer: Vec<u8>,
+    pub width: u32,
+    pub height: u32,
+}
+
+impl Image {
+    /// Create an image from a .png file.
+    pub fn from_png<P: AsRef<Path>>(path: P) -> Result<Self, Error> {
+        let decoder = Decoder::new(BufReader::new(
+            File::open(path.as_ref())
+                .map_err(|e| Error::OpenFile(e, path.as_ref().to_path_buf()))?,
+        ));
+        let mut reader = decoder
+            .read_info()
+            .map_err(|e| Error::Decode(e, path.as_ref().to_path_buf()))?;
+        let mut buffer = vec![
+            0;
+            reader
+                .output_buffer_size()
+                .ok_or(Error::BufferSize(path.as_ref().to_path_buf()))?
+        ];
+        // Read the next frame. An APNG might contain multiple frames.
+        let info = reader
+            .next_frame(&mut buffer)
+            .map_err(|e| Error::Decode(e, path.as_ref().to_path_buf()))?;
+        let buffer = Self::convert(
+            path.as_ref(),
+            &buffer[..info.buffer_size()],
+            info.color_type,
+        )?;
+        // Grab the bytes of the image.
+        Ok(Self {
+            width: info.width,
+            height: info.height,
+            buffer,
+        })
+    }
+
+    fn convert(path: &Path, buffer: &[u8], color_type: ColorType) -> Result<Vec<u8>, Error> {
+        match color_type {
+            ColorType::Rgb => Ok(buffer.to_vec()),
+            ColorType::Indexed => Err(Error::Indexed(path.to_path_buf())),
+            ColorType::Rgba => Ok(Self::rgba_to_rgb(buffer)),
+            ColorType::Grayscale => Ok(Self::grayscale_to_rgb(buffer)),
+            ColorType::GrayscaleAlpha => Ok(Self::grayscale_alpha_to_rgb(buffer)),
+        }
+    }
+
+    fn grayscale_to_rgb(buffer: &[u8]) -> Vec<u8> {
+        let mut out = vec![[0; 3]; buffer.len() * 3];
+        for (src, dst) in buffer.iter().zip(out.iter_mut()) {
+            let src = *src;
+            *dst = [src, src, src];
+        }
+        cast_slice::<[u8; 3], u8>(&out).to_vec()
+    }
+
+    fn grayscale_alpha_to_rgb(buffer: &[u8]) -> Vec<u8> {
+        let bytes = cast_slice::<u8, [u8; 2]>(buffer);
+        let mut out = vec![[0; 3]; bytes.len()];
+        for (src, dst) in bytes.iter().zip(out.iter_mut()) {
+            let src = src[0];
+            *dst = [src, src, src];
+        }
+        cast_slice::<[u8; 3], u8>(&out).to_vec()
+    }
+
+    fn rgba_to_rgb(buffer: &[u8]) -> Vec<u8> {
+        let bytes = cast_slice::<u8, [u8; 4]>(buffer);
+        let mut out = vec![[0; 3]; bytes.len()];
+        for (src, dst) in bytes.iter().zip(out.iter_mut()) {
+            *dst = [src[0], src[1], src[2]];
+        }
+        cast_slice::<[u8; 3], u8>(&out).to_vec()
+    }
+}
