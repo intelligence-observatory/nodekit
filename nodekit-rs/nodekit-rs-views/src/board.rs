4c560188c0cc4a62dd1fa1cf9d49c388a2b36df6
Esther Alter <alters@mit.edu>
2025-11-14 10:56:27 -0500

    view

---
+++ nodekit-rs/nodekit-rs-views/src/board.rs
@@ -0,0 +1,47 @@
+//! Various constants used to describe the size of the visual board.
+
+use blittle::*;
+
+pub const BOARD_D: usize = 768;
+pub const BOARD_D_U32: u32 = 768;
+pub const BOARD_D_ISIZE: isize = 768;
+pub const BOARD_D_F64: f64 = 768.;
+pub const BOARD_D_F64_HALF: f64 = 384.;
+pub const BOARD_SIZE: Size = Size {
+    w: BOARD_D,
+    h: BOARD_D,
+};
+pub const STRIDE: usize = stride::RGB;
+
+/// Convert a value between -0.5 and 0.5 into a pixel coordinate.
+pub const fn spatial_coordinate(c: f64) -> isize {
+    (BOARD_D_F64_HALF + BOARD_D_F64 * c) as isize
+}
+
+/// Convert a value between 0 and 1 into a pixel coordinate.
+pub const fn size_coordinate(c: f64) -> usize {
+    (BOARD_D_F64 * c) as usize
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn test_board_constants() {
+        assert_eq!(BOARD_D, BOARD_D_U32 as usize);
+        assert_eq!(BOARD_D, BOARD_D_ISIZE as usize);
+        assert_eq!(BOARD_D, BOARD_D_F64 as usize);
+        assert_eq!(BOARD_D / 2, BOARD_D_F64_HALF as usize);
+    }
+
+    #[test]
+    fn test_coordinates() {
+        assert_eq!(spatial_coordinate(0.), BOARD_D_ISIZE / 2);
+        assert_eq!(spatial_coordinate(-0.5), 0);
+        assert_eq!(spatial_coordinate(0.5), BOARD_D_ISIZE);
+
+        assert_eq!(size_coordinate(0.), 0);
+        assert_eq!(size_coordinate(1.), BOARD_D);
+    }
+}
---
+++ nodekit-rs/nodekit-rs-views/src/visual_buffer.rs
@@ -0,0 +1,91 @@
+use blittle::*;
+use blittle::stride::RGB;
+use fast_image_resize::{FilterType, PixelType, ResizeAlg, ResizeOptions, Resizer, SrcCropping};
+use nodekit_rs_models::Rect;
+use crate::board::*;
+use crate::error::Error;
+
+/// A raw RGB24 bitmap and its pixel size.
+pub struct VisualBuffer {
+    /// A raw RGB24 bitmap.
+    pub buffer: Vec<u8>,
+    pub width: u32,
+    pub height: u32,
+}
+
+impl VisualBuffer {
+    /// Resize to fit within the bounds of the rect.
+    /// Then, blit my buffer onto the board.
+    pub fn blit(&mut self, rect: Rect, board: &mut [u8]) -> Result<(), Error> {
+        let (rect, len) = self.resize_to_fit(rect)?;
+        // Clip.
+        let position = PositionI {
+            x: spatial_coordinate(rect.position.x),
+            y: spatial_coordinate(rect.position.y),
+        };
+        let mut src_size = Size {
+            w: size_coordinate(rect.size.w),
+            h: size_coordinate(rect.size.h),
+        };
+        let position = clip(&position, &BOARD_SIZE, &mut src_size);
+        blit(&self.buffer[0..len], &src_size, board, &position, &BOARD_SIZE, RGB);
+        Ok(())
+    }
+
+    /// Resize to fit within the bounds of `(width, height)`.
+    fn resize_to_fit(&mut self, mut rect: Rect) -> Result<(Rect, usize), Error> {
+        // Constrain to the bounds.
+        let (width, height) = if rect.size.w < rect.size.h {
+            (rect.size.w, rect.size.h)
+        } else if rect.size.w < rect.size.h {
+            (rect.size.w, rect.size.w * rect.size.w / rect.size.h)
+        }
+        else {
+            (rect.size.h, rect.size.h * rect.size.h / rect.size.w)
+        };
+        // Convert to pixel units.
+        let dst_w = size_coordinate(width);
+        let dst_h = size_coordinate(height);
+
+        // Create an image view.
+        let src = fast_image_resize::images::Image::from_slice_u8(
+            self.width,
+            self.height,
+            &mut self.buffer,
+            PixelType::U8x3,
+        )
+            .map_err(Error::ImageResizeBuffer)?;
+
+        // Resize the image.
+        let mut dst = fast_image_resize::images::Image::new(dst_w as u32, dst_h as u32, PixelType::U8x3);
+        let options = ResizeOptions {
+            algorithm: ResizeAlg::Convolution(FilterType::Bilinear),
+            cropping: SrcCropping::None,
+            mul_div_alpha: false,
+        };
+        let mut resizer = Resizer::new();
+        resizer
+            .resize(&src, &mut dst, Some(&options))
+            .map_err(Error::ImageResize)?;
+
+        // Offset the position.
+        if width < rect.size.w {
+            rect.position.x += rect.size.w / 2. - width / 2.;
+        }
+        if height < rect.size.h {
+            rect.position.y += rect.size.h / 2. - height / 2.;
+        }
+        // Set the size.
+        rect.size.w = width;
+        rect.size.h = height;
+
+        // Set the new buffer.
+        let dst_buffer = dst.buffer();
+        let dst_len = dst_buffer.len();
+        if dst_len > self.buffer.len() {
+            self.buffer.resize(dst_len, 0);
+        }
+        self.buffer[0..dst_len].copy_from_slice(dst_buffer);
+        Ok((rect, dst_len))
+    }
+}
