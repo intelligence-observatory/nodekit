64e4e8353353a4fbc8f86190c6efa373da031f47
Esther Alter <alters@mit.edu>
2025-11-14 14:51:13 -0500

    rgba image

--- nodekit-rs/nodekit-rs-image/src/lib.rs
+++ nodekit-rs/nodekit-rs-image/src/lib.rs
@@ -38,7 +38,7 @@ fn load(image: &Image) -> Result<VisualBuffer, Error> {
     let info = reader
         .next_frame(&mut buffer)
         .map_err(|e|  Error::Decode(e, image.path.clone()))?;
-    // Convert to RGB24.
+    // Convert to RGB32.
     let buffer = convert(
         &image.path,
         &buffer[..info.buffer_size()],
@@ -54,40 +54,40 @@ fn load(image: &Image) -> Result<VisualBuffer, Error> {
 /// Convert `buffer` from a `color_type` to RGB24.
 fn convert(path: &Path, buffer: &[u8], color_type: ColorType) -> Result<Vec<u8>, Error> {
     match color_type {
-        ColorType::Rgb => Ok(buffer.to_vec()),
+        ColorType::Rgb => Ok(rgb_to_rgba(buffer)),
         ColorType::Indexed => Err(Error::Indexed(path.to_path_buf())),
-        ColorType::Rgba => Ok(rgba_to_rgb(buffer)),
-        ColorType::Grayscale => Ok(grayscale_to_rgb(buffer)),
-        ColorType::GrayscaleAlpha => Ok(grayscale_alpha_to_rgb(buffer)),
+        ColorType::Rgba => Ok(buffer.to_vec()),
+        ColorType::Grayscale => Ok(grayscale_to_rgba(buffer)),
+        ColorType::GrayscaleAlpha => Ok(grayscale_alpha_to_rgba(buffer)),
     }
 }
 
-fn grayscale_to_rgb(buffer: &[u8]) -> Vec<u8> {
-    let mut out = vec![[0; 3]; buffer.len() * 3];
-    for (src, dst) in buffer.iter().zip(out.iter_mut()) {
+fn grayscale_to_rgba(buffer: &[u8]) -> Vec<u8> {
+    let mut dst = vec![[0; STRIDE]; buffer.len() * STRIDE];
+    for (src, dst) in buffer.iter().zip(dst.iter_mut()) {
         let src = *src;
-        *dst = [src, src, src];
+        *dst = [src, src, src, 255];
     }
-    cast_slice::<[u8; 3], u8>(&out).to_vec()
+    cast_slice::<[u8; STRIDE], u8>(&dst).to_vec()
 }
 
-fn grayscale_alpha_to_rgb(buffer: &[u8]) -> Vec<u8> {
-    let bytes = cast_slice::<u8, [u8; 2]>(buffer);
-    let mut out = vec![[0; 3]; bytes.len()];
-    for (src, dst) in bytes.iter().zip(out.iter_mut()) {
-        let src = src[0];
-        *dst = [src, src, src];
+fn grayscale_alpha_to_rgba(buffer: &[u8]) -> Vec<u8> {
+    let src = cast_slice::<u8, [u8; 2]>(buffer);
+    let mut dst = vec![[0; STRIDE]; src.len()];
+    for (src, dst) in src.iter().zip(dst.iter_mut()) {
+        let c = src[0];
+        *dst = [c, c, c, src[1]];
     }
-    cast_slice::<[u8; 3], u8>(&out).to_vec()
+    cast_slice::<[u8; STRIDE], u8>(&dst).to_vec()
 }
 
-fn rgba_to_rgb(buffer: &[u8]) -> Vec<u8> {
-    let bytes = cast_slice::<u8, [u8; 4]>(buffer);
-    let mut out = vec![[0; 3]; bytes.len()];
-    for (src, dst) in bytes.iter().zip(out.iter_mut()) {
-        *dst = [src[0], src[1], src[2]];
+fn rgb_to_rgba(buffer: &[u8]) -> Vec<u8> {
+    let src = cast_slice::<u8, [u8; 3]>(buffer);
+    let mut dst = vec![[0; STRIDE]; src.len()];
+    for (src, dst) in src.iter().zip(dst.iter_mut()) {
+        *dst = [src[0], src[1], src[2], 255];
     }
-    cast_slice::<[u8; 3], u8>(&out).to_vec()
+    cast_slice::<[u8; STRIDE], u8>(&dst).to_vec()
 }
 
 #[cfg(test)]
@@ -104,6 +104,6 @@ mod tests {
         }).unwrap();
         assert_eq!(image.width, width);
         assert_eq!(image.height, height);
-        assert_eq!(image.buffer.len(), (width * height * 3) as usize);
+        assert_eq!(image.buffer.len(), (width * height * STRIDE as u32) as usize);
     }
 }
