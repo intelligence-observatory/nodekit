9225989dfd2ebaca8ea2f4c85d4aaa9ce12f4a49
Esther Alter <alters@mit.edu>
2025-11-18 10:29:43 -0500

    seeking worksgit status

--- nodekit-rs/nodekit-rs-video/src/lib.rs
+++ nodekit-rs/nodekit-rs-video/src/lib.rs
@@ -1,15 +1,15 @@
 mod error;
 
 pub use error::Error;
-use scuffle_ffmpeg::decoder::{Decoder, VideoDecoder};
-use scuffle_ffmpeg::io::Input;
-use scuffle_ffmpeg::scaler::VideoScaler;
-use scuffle_ffmpeg::{AVMediaType, AVPixelFormat};
+use scuffle_ffmpeg::{
+    decoder::Decoder,
+    io::Input,
+    scaler::VideoScaler,
+    AVMediaType,
+    AVPixelFormat,
+    frame::VideoFrame
+};
 use std::io::Cursor;
-use std::ops::{Deref, DerefMut};
-use scuffle_ffmpeg::consts::Const;
-use scuffle_ffmpeg::frame::VideoFrame;
-use scuffle_ffmpeg::stream::Stream;
 
 pub fn get_frame(buffer: &[u8], t_msec: u64, width: u32, height: u32) -> Result<Vec<u8>, Error> {
     // Open the buffer.
@@ -27,30 +27,28 @@ pub fn get_frame(buffer: &[u8], t_msec: u64, width: u32, height: u32) -> Result<
         .map_err(Error::Ffmpeg)?
         .video()
         .map_err(|_| Error::NotVideoDecoder)?;
-    let frame_rate = 1. / video.r_frame_rate().as_f64() * 1000.;
-    let mut t0 = 0.;
-    let t1 = t_msec as f64;
-    let mut frame = None;
-    while t0 <= t1 {
-        while frame.is_none() {
-            let packet = input.receive_packet().unwrap().unwrap();
-            if packet.stream_index() == video_index {
-                video_decoder.send_packet(&packet).map_err(Error::Ffmpeg)?;
-                while let Ok(Some(f)) = video_decoder.receive_frame().map_err(Error::Ffmpeg) {
-                    frame = Some(f);
-                    t0 += frame_rate;
-                    if t0 >= t1 {
-                        break;
-                    }
-                };
-            }
-            if t0 >= t1 {
-                break;
-            }
+    let mut frame_index = 0;
+    let target_frame_index = (t_msec as f64 / 1000. * video.r_frame_rate().as_f64()) as usize;
+    let mut frame: Option<VideoFrame> = None;
+    let mut got_frame = false;
+    for packet in input.packets() {
+        let packet = packet.map_err(Error::Ffmpeg)?;
+        if packet.stream_index() == video_index {
+            video_decoder.send_packet(&packet).map_err(Error::Ffmpeg)?;
+            while let Ok(Some(f)) = video_decoder.receive_frame().map_err(Error::Ffmpeg) {
+                frame = Some(f);
+                frame_index += 1;
+                if frame_index >= target_frame_index {
+                    got_frame = true;
+                    break
+                }
+            };
+        }
+        if got_frame {
+            break;
         }
     }
     let frame = frame.ok_or(Error::NoFrame(t_msec))?;
-    println!("{:?}", frame.best_effort_timestamp());
     let mut scaler = VideoScaler::new(
         frame.width() as i32,
         frame.height() as i32,
