dbb84bcfccd2c054467f91097d96d79119ca8ef2
Esther Alter <alters@mit.edu>
2025-11-20 12:23:12 -0500

    bench

---
+++ nodekit-rs/nodekit-rs-visual/benches/bench.rs
@@ -0,0 +1,38 @@
+use criterion::{Criterion, criterion_group, criterion_main};
+use blittle::PositionU;
+use bytemuck::cast_slice;
+use nodekit_rs_visual::*;
+use nodekit_rs_models::Rect;
+
+fn get_rgb_buffer() -> Vec<u8> {
+    cast_slice::<u8, [u8; 4]>(include_bytes!("../test_image.raw"))
+        .into_iter()
+        .map(|pixel| [pixel[0], pixel[1], pixel[2]])
+        .flatten()
+        .collect::<Vec<u8>>()
+}
+
+fn criterion_benchmark(c: &mut Criterion) {
+    let mut board = board([255, 0, 255]);
+
+    let visual = VisualBuffer {
+        buffer: get_rgb_buffer(),
+        rect: BlitRect {
+            position: PositionU::default(),
+            size: blittle::Size { w: 300, h: 600 },
+        },
+    };
+
+    c.bench_function("blit visual buffer", |b| {
+        b.iter(|| visual.blit(&mut board))
+    });
+
+    let buffer = get_rgb_buffer();
+    let rect = Rect::new(-0.5, -0.5, 1., 1.);
+    c.bench_function("resize visual buffer", |b| {
+        b.iter(|| VisualBuffer::new_resized(buffer.clone(), 300, 600, rect))
+    });
+}
+
+criterion_group!(benches, criterion_benchmark);
+criterion_main!(benches);
