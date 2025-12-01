37d0d6ed9e50cb43825922c0a3efc73e32022863
himjl <mil@mit.edu>
2025-12-01 16:03:58 -0500

    fix examp

--- nodekit-browser/src/board-view/board-view.ts
+++ nodekit-browser/src/board-view/board-view.ts
@@ -178,7 +178,7 @@ export function createRegionDiv(
             break
         default:
             const _exhaustive: never = region.mask
-            console.warn(`Found unsupported Region.mask: ${JSON.stringify(_exhaustive)}`)
+            console.warn(`Found unsupported Region.mask: ${JSON.stringify(region)}`)
             break
     }
 
