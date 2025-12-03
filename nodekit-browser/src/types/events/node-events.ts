a84a9b555f6ef87607868c91bba5aa13ba658bc4
himjl <mil@mit.edu>
2025-12-02 22:36:31 -0500

    unused

--- nodekit-browser/src/types/events/node-events.ts
+++
@@ -1,8 +0,0 @@
-import type {NodeId, TimeElapsedMsec} from "../common.ts";
-import type {Action} from "../actions";
-
-export interface NodeOutcome {
-    t: TimeElapsedMsec // Start time of the Node, relative to Trace start
-    node_id: NodeId
-    action: Action // Final, rolled up sensor values
-}
