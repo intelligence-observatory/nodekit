007c4be21c5939fa24c7fde13236d5bf583bdfe4
himjl <mil@mit.edu>
2025-12-04 00:23:07 -0500

    interpreter stub

---
+++ nodekit/_internal/types/expressions/interpreter.py
@@ -0,0 +1,12 @@
+from nodekit._internal.types.expressions.expressions import Expression
+from nodekit._internal.types.value import (
+    Value,
+    List,
+    RegisterId
+)
+
+from nodekit._internal.types.actions.actions import (
+    Action
+)
+
+
