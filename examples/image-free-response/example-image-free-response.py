0bacab2dc25d64f3dd8ee65a2cf5a8a67c8991be
himjl <mil@mit.edu>
2025-12-07 03:34:13 -0500

    image free response

--- examples/image-free-response/example-image-free-response.py
+++ examples/image-free-response/example-image-free-response.py
@@ -4,46 +4,37 @@ import nodekit as nk
 def make_free_survey(image: nk.assets.Image, prompt: str) -> nk.Node:
     image_card = nk.cards.ImageCard(
         image=image,
-        x=0,
-        y=0.2,
-        h=0.6,
-        w=1,
+        region=nk.Region(
+            x=0,
+            y=0.2,
+            h=0.6,
+            w=1,
+        ),
     )
 
-    prompt_card = nk.cards.TextCard(text=prompt, x=0, y=-0.15, h=0.1, w=0.6)
-
-    free_response_card = nk.cards.FreeTextEntryCard(
-        prompt="Enter your answer here",
-        x=0,
-        y=-0.3,
-        w=prompt_card.w,
-        h=0.2,
-    )
-
-    submit_button = nk.cards.TextCard(
-        x=0,
-        y=-0.45,
-        h=0.04,
-        w=free_response_card.w / 2,
-        font_size=0.02,
-        text="Submit response",
-        selectable=True,
-        background_color="#e6e6e6",
+    prompt_card = nk.cards.TextCard(
+        text=prompt,
+        region=nk.Region(
+            x=0, y=-0.15, h=0.1, w=0.6
+        ),
     )
 
     return nk.Node(
-        cards={
-            "stimulus": image_card,
-            "prompt_card": prompt_card,
-            "free-response": free_response_card,
-            "submit-button": submit_button,
-        },
-        sensors={
-            "submit": nk.sensors.SubmitSensor(
-                source_ids=["free-response"],
-                submitter_id="submit-button",
-            )
-        },
+        stimulus=nk.cards.CompositeCard(
+            children={
+                "image": image_card,
+                "prompt": prompt_card,
+            }
+        ),
+        sensor=nk.sensors.FreeTextEntrySensor(
+            prompt="Enter your answer here",
+            region=nk.Region(
+                x=0,
+                y=-0.3,
+                w=prompt_card.region.w,
+                h=0.2,
+            ),
+        ),
         board_color="#ffffff",
     )
 
