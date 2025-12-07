bfd5f0789a8f059a37198bb357d4f4c315ec921b
himjl <mil@mit.edu>
2025-12-07 02:14:08 -0500

    fix big 5

--- examples/big-5-inventory/my_big_5.py
+++ examples/big-5-inventory/my_big_5.py
@@ -24,54 +24,48 @@ def make_big5_node(
         )
 
     question_card = nk.cards.TextCard(
-        x=0,
-        y=0.25,
-        w=1,
-        h=0.4,
         text=prompt,
         justification_horizontal="center",
         justification_vertical="center",
-        start_msec=0,
         font_size=0.04,
+        region=nk.Region(
+            x=0,
+            y=0.25,
+            w=1,
+            h=0.4,
+        ),
     )
 
     num_bins = len(ordinal_choice_descriptions)
-    slider_card = nk.cards.SliderCard(
+    slider_sensor = nk.sensors.SliderSensor(
         num_bins=num_bins,
         initial_bin_index=initial_index,
         orientation="horizontal",
         show_bin_markers=True,
-        x=0,
-        y=-0.1,
-        w=0.75,
-        h=0.1,
+        region=nk.Region(
+            x=0,
+            y=-0.1,
+            w=0.75,
+            h=0.1,
+        ),
     )
 
     cards = {
         "question": question_card,
-        "likert-scale": slider_card,
-        "submit-button": nk.cards.TextCard(
-            selectable=True,
-            x=0,
-            y=-0.475,
-            w=0.25,
-            h=0.05,
-            text="**Next**",
-            background_color="#d0d2d6",
-            start_msec=1000,
-        ),
     }
 
     # Assemble annotation cards
-    annotation_box_width = slider_card.w / (num_bins - 1)
-    slider_left = slider_card.x - slider_card.w / 2
+    annotation_box_width = slider_sensor.region.w / (num_bins - 1)
+    slider_left = slider_sensor.region.x - slider_sensor.region.w / 2
 
     for i, description in enumerate(ordinal_choice_descriptions):
         cards[f"likert-description-{i}"] = nk.cards.TextCard(
-            x=slider_left + (i) * annotation_box_width,
-            y=slider_card.y + slider_card.h / 2 + 0.05,
-            w=annotation_box_width,
-            h=0.1,
+            region=nk.Region(
+                x=slider_left + (i) * annotation_box_width,
+                y=slider_sensor.region.y + slider_sensor.region.h / 2 + 0.05,
+                w=annotation_box_width,
+                h=0.1,
+            ),
             text=description,
             font_size=0.02,
             justification_horizontal="center",
@@ -79,15 +73,14 @@ def make_big5_node(
             background_color="#E6E6E600",  # Transparent
         )
 
+    stimulus = nk.cards.CompositeCard(
+        children=cards
+    )
+
     # Assemble Node
     node = nk.Node(
-        cards=cards,
-        sensors={
-            "submit-likert": nk.sensors.SubmitSensor(
-                submitter_id="submit-button",
-                source_ids=["likert-scale"],
-            )
-        },
+        stimulus=stimulus,
+        sensor=slider_sensor,
         board_color="#ffffff",
     )
 
