66e055db60960f284916f8c179534bd0a7a4d895
himjl <mil@mit.edu>
2025-12-14 18:42:29 -0500

    validating transition

--- nodekit/_internal/types/graph.py
+++ nodekit/_internal/types/graph.py
@@ -29,8 +29,10 @@ class Graph(pydantic.BaseModel):
     def check_graph_is_valid(
         self,
     ) -> Self:
+
         if self.start not in self.nodes:
             raise ValueError(f"Start Node {self.start} does not exist in nodes.")
+
         num_nodes = len(self.nodes)
         if num_nodes == 0:
             raise ValueError("Graph must have at least one node.")
@@ -42,7 +44,12 @@ class Graph(pydantic.BaseModel):
                     f"Node {node_id} exists but has no corresponding Transition."
                 )
 
-        # Todo: Each Node must be reachable from the start Node (i.e., no disconnected components)
+        # Check each Transition corresponds to an existing Node
+        for node_id in self.transitions:
+            if node_id not in self.nodes:
+                raise ValueError(
+                    f"Transition for Node {node_id} but Node does not exist."
+                )
 
         # Each Go transition must point to an existing Node
         def go_targets(transition: Transition) -> list[NodeId]:
@@ -58,12 +65,6 @@ class Graph(pydantic.BaseModel):
                 return targets
             return []
 
-        for node_id in self.transitions:
-            if node_id not in self.nodes:
-                raise ValueError(
-                    f"Transition for Node {node_id} but Node does not exist."
-                )
-
         for node_id, transition in self.transitions.items():
             for to_node_id in go_targets(transition):
                 if to_node_id not in self.nodes:
@@ -72,5 +73,34 @@ class Graph(pydantic.BaseModel):
                     )
 
         # Todo: check each Nodes has a path to an End transition
+        # Todo: Each Node must be reachable from the start Node (i.e., no disconnected components)
 
         return self
+
+# %%
+def _validate_transition(
+        transition: Transition,
+        graph_element: Node | Graph,
+        registers: Dict[RegisterId, Value],
+) -> None:
+    """
+    This function validates that the given transition is valid for the given graph child and and graph registers.
+    For example, if the Transition references a RegisterId, it checks that the RegisterId exists in the registers dict.
+
+    Args:
+        transition: the Transition to validate.
+        graph_element: a Node or a subGraph.
+        registers: the enclosing Graph's registers.
+
+    Returns:
+        None
+
+    """
+
+    match transition.transition_type:
+        case "Go" | "End":
+            register_updates = transition.register_updates
+
+
+    # Todo:
+
