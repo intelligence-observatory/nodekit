1e826a18306ab450543e1fdd134c0e6ddaac695b
himjl <mil@mit.edu>
2025-12-21 00:04:32 -0800

    submission types

--- nodekit/_internal/ops/build_site/types.py
+++ nodekit/_internal/ops/build_site/types.py
@@ -4,52 +4,50 @@ import pydantic
 
 from nodekit._internal.types.trace import Trace
 
+# %%
+class BasePlatformContext(pydantic.BaseModel):
+    platform: str
+
 
 # %%
-class BaseTraceSubmission(pydantic.BaseModel):
-    platform: str = pydantic.Field(
-        description="The name of the platform."
-    )
-    trace: Trace = pydantic.Field(
-        description="The submitted Trace."
-    )
-    
-# %% 
-class MechanicalTurkTraceSubmission(BaseTraceSubmission):
-    platform: Literal['MechanicalTurk']
-    worker_id: str = pydantic.Field(
-        description="The Mechanical Turk Worker ID."
-    )
-    hit_id: str = pydantic.Field(
-        description="The Mechanical Turk HIT ID."
-    )
-    assignment_id: str = pydantic.Field(
-        description="The Mechanical Turk Assignment ID."
-    )
+class MechanicalTurkContext(BasePlatformContext):
+    platform: Literal["MechanicalTurk"]
+    assignment_id: str = pydantic.Field(description="The Mechanical Turk Assignment ID.")
+    worker_id: str = pydantic.Field(description="The Mechanical Turk Worker ID.")
+    hit_id: str = pydantic.Field(description="The Mechanical Turk HIT ID.")
+    turk_submit_to: str = pydantic.Field(description="The link that the Trace was submitted to. Encodes whether sandbox or production.")
+
+
 
 # %%
-class ProlificTraceSubmission(BaseTraceSubmission):
-    platform: Literal['Prolific']
-    participant_id: str = pydantic.Field(
-        description="The Prolific Participant ID."
-    )
-    study_id: str = pydantic.Field(
-        description="The Prolific Study ID."
-    )
-    session_id: str = pydantic.Field(
-        description="The Prolific Session ID."
-    )
+class ProlificContext(BasePlatformContext):
+    platform: Literal["Prolific"]
+    completion_code: str = pydantic.Field(description="The Prolific Completion Code.")
+    prolific_pid: str = pydantic.Field(description="The Prolific Participant ID.")
+    study_id: str = pydantic.Field(description="The Prolific Study ID.")
+    session_id: str = pydantic.Field(description="The Prolific Session ID.")
+
 
 # %%
-class OtherTraceSubmission(BaseTraceSubmission):
-    platform: Literal['Other']
+class NoPlatformContext(BasePlatformContext):
+    platform: Literal["None"]
+
 
 # %%
-type TraceSubmission = Annotated[
+type PlatformContext = Annotated[
     Union[
-        MechanicalTurkTraceSubmission,
-        ProlificTraceSubmission,
-        OtherTraceSubmission,
+        MechanicalTurkContext,
+        ProlificContext,
+        NoPlatformContext,
     ],
-    pydantic.Field(discriminator="platform")
+    pydantic.Field(discriminator="platform"),
 ]
+
+
+
+# %%
+class SubmissionPayload(pydantic.BaseModel):
+    trace: Trace = pydantic.Field(description="The submitted Trace.")
+    platform_context: PlatformContext = pydantic.Field(description="Information about the platform (if any) that the Graph site was hosted on.")
+
+
