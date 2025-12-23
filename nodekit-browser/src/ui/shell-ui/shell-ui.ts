e18a82d5b017dcb3128bafa5da19f83fdeb6724b
himjl <mil@mit.edu>
2025-12-23 13:09:20 -0800

    removing pbar

--- nodekit-browser/src/ui/shell-ui/shell-ui.ts
+++ nodekit-browser/src/ui/shell-ui/shell-ui.ts
@@ -2,7 +2,6 @@ import {UIElementBase} from "./base.ts";
 
 import './shell-ui.css'
 
-import {ProgressBar} from "./progress-bar/progress-bar.ts";
 import {SessionConnectingOverlay} from "./overlays/session-connecting/session-connecting.ts";
 import {ConsoleMessageOverlay} from "./overlays/console-message/console-message.ts";
 import {SessionFinishedOverlay} from "./overlays/session-finished/session-finished.ts";
@@ -10,9 +9,6 @@ import {SessionStartedOverlay} from "./overlays/session-start/session-start.ts";
 
 export class ShellUI extends UIElementBase {
     root: HTMLDivElement
-
-    // Widgets:
-    private progressBar: ProgressBar
 
     // Overlays
     private sessionConnectingOverlay: SessionConnectingOverlay;
@@ -25,11 +21,6 @@ export class ShellUI extends UIElementBase {
         // Make a root div which will hold all Shell UI elements
         this.root = document.createElement("div");
         this.root.className = 'shell-ui';
-
-        // Initialize progress bar
-        this.progressBar = new ProgressBar('cognition');
-        this.progressBar.mount(this.root);
-
 
         // Initialize overlay for session connecting
         this.sessionConnectingOverlay = new SessionConnectingOverlay();
@@ -48,11 +39,6 @@ export class ShellUI extends UIElementBase {
         this.sessionStartedOverlay.mount(this.root);
 
     }
-
-    setProgressBar(percent: number) {
-        this.progressBar.setProgress(percent);
-    }
-
 
     showSessionConnectingOverlay(startDelayMsec: number = 500) {
         // Show the overlay
