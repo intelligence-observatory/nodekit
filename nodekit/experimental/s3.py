087a72c4e028bec47b2c807a075030990a1dbce8
himjl <mil@mit.edu>
2026-01-20 16:02:52 -0500

    remove unused methods

--- nodekit/experimental/s3.py
+++ nodekit/experimental/s3.py
@@ -1,17 +1,13 @@
-import pydantic
+import mimetypes
+import os
+from pathlib import Path
+from urllib.parse import quote
 
 import boto3
 import botocore.client
 import botocore.exceptions
-from typing import BinaryIO
-import hashlib
-import mimetypes
-
-from nodekit._internal.types.values import SHA256, MediaType
-import os
-from pathlib import Path
-
-from urllib.parse import quote
+import pydantic
+from nodekit._internal.types.values import MediaType, SHA256
 
 
 # %%
@@ -23,6 +19,9 @@ class UploadAssetResult(pydantic.BaseModel):
 
 class S3Client:
     class Config(pydantic.BaseModel):
+        """
+        Used to validate and store S3 client configuration.
+        """
         bucket_name: str
         region_name: str
         aws_access_key_id: str
@@ -46,110 +45,6 @@ class S3Client:
             region_name=self.config.region_name,
             aws_access_key_id=self.config.aws_access_key_id,
             aws_secret_access_key=self.config.aws_secret_access_key.get_secret_value(),
-        )
-
-    @staticmethod
-    def _derive_s3_key(
-        sha256: SHA256,
-        mime_type: MediaType,
-    ) -> str:
-        ext = mimetypes.guess_extension(mime_type)
-        if ext is None:
-            raise ValueError(f"Could not determine file extension for mime type {mime_type}")
-        return f"assets/{mime_type}/{sha256}{ext}"
-
-    def _assemble_s3_url(self, key: str) -> pydantic.HttpUrl:
-        url = f"https://{self.config.bucket_name}.s3.{self.config.region_name}.amazonaws.com/{key}"
-        return pydantic.HttpUrl(url)
-
-    def maybe_resolve_asset(
-        self,
-        sha256: SHA256,
-        mime_type: MediaType,
-    ) -> UploadAssetResult | None:
-        # Derive S3 key
-        key = self._derive_s3_key(sha256=sha256, mime_type=mime_type)
-
-        # Check if it exists
-        try:
-            self._client.head_object(Bucket=self.config.bucket_name, Key=key)
-        except botocore.exceptions.ClientError as e:
-            if e.response["Error"]["Code"] == "404":
-                return None
-            else:
-                raise RuntimeError(
-                    f"S3 head_object failed: {e.response.get('Error', {}).get('Message', str(e))}"
-                ) from e
-
-        # Return resolved asset ref
-        return UploadAssetResult(
-            mime_type=mime_type,
-            sha256=sha256,
-            asset_url=self._assemble_s3_url(key),
-        )
-
-    def upload_asset(
-        self,
-        claimed_sha256: SHA256,
-        file: BinaryIO,
-        mime_type: MediaType,
-    ) -> UploadAssetResult:
-        """
-        Stream `file` directly to S3 with the given MIME type, and returns it public link.
-        """
-        # ---- derive a safe S3 key
-        ext = mimetypes.guess_extension(mime_type)
-        if ext is None:
-            raise ValueError(f"Could not determine file extension for mime type {mime_type}")
-
-        # First I/O pass: compute SHA256
-        try:
-            file.seek(0)
-        except Exception:
-            pass
-
-        sha256 = hashlib.sha256()
-        try:
-            file.seek(0)
-        except Exception:
-            pass
-        while True:
-            chunk = file.read(8192)  # 8 MB
-            if not chunk:
-                break
-            sha256.update(chunk)
-        sha256 = sha256.hexdigest()
-
-        if not claimed_sha256 == sha256:
-            raise ValueError(f"SHA256 mismatch: claimed {claimed_sha256}, computed {sha256}")
-
-        # Return immediately if already uploaded:
-        key = self._derive_s3_key(sha256=sha256, mime_type=mime_type)
-        asset_url = self._assemble_s3_url(key)
-
-        # Second I/O pass: upload to S3
-        file.seek(0)
-
-        try:
-            self._client.upload_fileobj(
-                Fileobj=file,
-                Bucket=self.config.bucket_name,
-                Key=key,
-                ExtraArgs={
-                    "ContentType": str(mime_type),
-                    "ACL": "public-read",
-                },
-            )
-        except botocore.exceptions.ClientError as e:
-            raise RuntimeError(
-                f"S3 upload failed: {e.response.get('Error', {}).get('Message', str(e))}"
-            ) from e
-
-        # Package return model
-        return UploadAssetResult(
-            sha256=sha256,
-            asset_url=asset_url,
-            mime_type=mime_type,
         )
 
     def sync_file(
